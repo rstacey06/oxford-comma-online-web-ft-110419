1dd310d64582c155dfea6a63f1b81200d791bbcf
Burbage Stacey <rstacey06@gmail.com>
2019-11-07 14:13:00 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,20 @@
 def oxford_comma(array)
+  if array.length == 1 
+    array.join
+    elsif array.length == 2 
+    array.join(" and ")
+  else array.length == 3 
+    array.insert(2, "and")
+  end 
+end
+
 if array.length == 1
   array.join
 elsif array.length == 2
-  array[] << (" and ")
+  array[0] << (" and ")
   array.join
 elsif array.length > 2
-  array[-1].insert(0, "and ")
+  array[-1].prepend "and "
   array.join(", ")
   end
 end
