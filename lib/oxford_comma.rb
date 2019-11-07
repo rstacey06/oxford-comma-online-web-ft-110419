55c06d9fcc021f65a4cffdfd3eaad48b445bdac2
Burbage Stacey <rstacey06@gmail.com>
2019-11-07 14:09:05 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
 if array.length == 1
   array.join
 elsif array.length == 2
-  array << (" and ")
+  array.join << (" and ")
   array.join
 elsif array.length > 2
   array[-1].prepend ("and ")
