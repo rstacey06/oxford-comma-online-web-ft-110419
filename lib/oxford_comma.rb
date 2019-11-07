cac45f468002083d213d5b5c076f31b6f303550f
Burbage Stacey <rstacey06@gmail.com>
2019-11-07 14:15:54 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,8 +3,8 @@ def oxford_comma(array)
     array.join
     elsif array.length == 2 
     array.join(" and ")
-    elsif array.length > 2
-  array[-1].insert(0, "and ")
+    elsif array.length == 3
+  array.insert(0, "and ")
   array.join(", ")
   end
 end
